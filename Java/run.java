9cfe3ad30abd3f973943fc56067bc4aaa57ef83e
CryosisOS <programmingonline4evs@gmail.com>
2018-03-17 20:18:29 +0800

    Initial
    Signed-off-by: CryosisOS <programmingonline4evs@gmail.com>

---
+++ Java/run.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Nathan van der Velde
+ * Date Created: 2018-03-17
+ * Description: This is the main class, where the main method is located.
+ */
+
+public class run
+{
+    public static void main(String [] args)
+    {
+    
+    }//END main
+}//END run
